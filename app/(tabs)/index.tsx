4643592c36bd5db387f76cdd9d45fc685d5a10ef
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2026-07-07 12:44:43 +0800

    Move the hidden route into tabs groups

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
+import { HOME_USER } from "@/constants/data";
+import images from "@/constants/images";
 import "@/global.css";
-import { Link } from "expo-router";
 import { styled } from "nativewind";
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView);
@@ -9,27 +10,12 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-7xl font-sans-extrabold text-primary">Home</Text>
-      <Link
-        href="/(auth)/sign-in"
-        className="bg-primary text-white text-l font-sans-bold p-3 rounded-md mt-5"
-      >
-        Sign In
-      </Link>
-      <Link
-        href="/(auth)/sign-up"
-        className="bg-primary text-white text-l font-sans-bold p-3 rounded-md mt-4"
-      >
-        Sign Up
-      </Link>
-
-      {/* Subscription Examples */}
-      <Link
-        href={{ pathname: "/subscriptions/[id]", params: { id: "claude-ai" } }}
-        className="bg-primary text-white text-l font-sans-bold p-3 rounded-md mt-30"
-      >
-        Claude Max Subscription
-      </Link>
+      <View className="home-header">
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar" />
+          <Text className="home-user-name">{HOME_USER.name}</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
